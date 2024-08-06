0f977cac295e4800655ef1475aef142f12a11506
q93o4f <q93o4f@shinsegae.com>
2024-08-06 15:50:02 +0900

    add worksop dir

--- infra/10. ec2.tf
+++ infra/10. ec2.tf
@@ -17,6 +17,28 @@
    }  
 
    tags = {
-     Name                    = "${var.name}-ec2"
+     Name                    = "${var.name}-server-collector"
+     }
+ }
+ resource "aws_instance" "ec2-scouter-agent" {
+   ami                       = var.ec2_ami
+   instance_type             = var.ec2_type
+  #  associate_public_ip_address = true
+   subnet_id                 = aws_subnet.private[0].id
+   vpc_security_group_ids    = [aws_security_group.ec2.id]
+   key_name                  = var.ec2_key
+   disable_api_termination = true
+   user_data ="${file("user_data")}"
+
+   root_block_device {
+     volume_size = 8
+     volume_type = "gp3"
+     tags = {
+       "Name" = "${var.name}-ec2-ebs"
+     }
+   }  
+
+   tags = {
+     Name                    = "${var.name}-agent"
      }
  }
--- infra/11. sg.tf
+++ infra/11. sg.tf
@@ -25,3 +25,42 @@
    cidr_ipv4         = "0.0.0.0/0"
    ip_protocol       = "-1" # semantically equivalent to all ports
  }
+
+#####ec2-scouter-agent####
+
+ resource "aws_security_group" "ec2-scouter-agent" {
+   name        = "${var.name}-ec2-scouter-agent-sg"
+   description = "Allow My Access"
+   vpc_id      = aws_vpc.main.id
+
+   tags = {
+     Name = "${var.name}-ec2-scouter-agent-sg"
+   }
+   lifecycle {
+    create_before_destroy = true
+   }
+ }
+
+ resource "aws_vpc_security_group_ingress_rule" "ec2-scouter-agent-1022" {
+   security_group_id = aws_security_group.ec2-scouter-agent.id
+   cidr_ipv4         = "0.0.0.0/0"
+   from_port         = 1022
+   ip_protocol       = "tcp"
+   to_port           = 1022
+   description       = "Bespin_Office"
+ }
+
+  resource "aws_vpc_security_group_ingress_rule" "ec2-scouter-agent-6100" {
+   security_group_id = aws_security_group.ec2-scouter-agent.id
+   cidr_ipv4         = "0.0.0.0/0"
+   from_port         = 6100
+   ip_protocol       = "tcp"
+   to_port           = 6100
+   description       = "6100"
+ }
+
+ resource "aws_vpc_security_group_egress_rule" "ec2-scouter-agent" {
+   security_group_id = aws_security_group.ec2-scouter-agent.id
+   cidr_ipv4         = "0.0.0.0/0"
+   ip_protocol       = "-1" # semantically equivalent to all ports
+ } 
